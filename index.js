312d6cbb648da7b277a46d78234d60250d1d9ef6
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-09-11 11:46:16 +0200

    Exposes util classes on index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  App: require('src/app/App'),
+  AppEventEmitter: require('src/app/AppEventEmitter'),
+  BaseAction: require('src/action/BaseAction'),
+  ClassLoader: require('src/utils/ClassLoader'),
+  ProductFlavors: require('src/flavor/ProductFlavors'),
+  SoyTemplateEngine: require('src/template/SoyTemplateEngine'),
+  TemplateEngine: require('src/template/TemplateEngine')
+};
