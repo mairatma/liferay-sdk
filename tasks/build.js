2b233e249b703cfc2ac55da90f07211058024664
Eduardo Lundgren <eduardo.lundgren@liferay.com>
2014-09-28 19:11:00 -0700

    Emits updateResources on build instead of copy since build is now used on watch instead of copy

--- tasks/build.js
+++ tasks/build.js
@@ -2,7 +2,11 @@
 
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
+var AppEventEmitter = require('../src/app/AppEventEmitter');
 
 gulp.task('build', function(done) {
-  runSequence('copy', ['images', 'fonts', 'scripts', 'styles', 'templates'], done);
+  runSequence('copy', ['images', 'fonts', 'scripts', 'styles', 'templates'], function() {
+    AppEventEmitter.emit('resourcesUpdated');
+    done();
+  });
 });
--- tasks/copy.js
+++ tasks/copy.js
@@ -1,12 +1,8 @@
 'use strict';
 
 var gulp = require('gulp');
-var AppEventEmitter = require('../src/app/AppEventEmitter');
 
 gulp.task('copy', function() {
   return gulp.src('src/**/*.*')
-    .pipe(gulp.dest('dist'))
-    .on('end', function() {
-      AppEventEmitter.emit('resourcesUpdated');
-    });
+    .pipe(gulp.dest('dist'));
 });
