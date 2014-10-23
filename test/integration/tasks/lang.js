f865feeb7333086b2469b7fd85608ef5379ff0c0
Maira Bello <maira.araujo@liferay.com>
2014-10-23 17:58:04 -0300

    Adds unit test for `lang` gulp task

---
+++ test/integration/tasks/lang.js
@@ -0,0 +1,34 @@
+'use strict';
+
+var assert = require('assert');
+var fs = require('fs');
+var TestUtils = require('../fixture/TestUtils');
+
+describe('lang', function() {
+  before(function() {
+    TestUtils.before();
+
+    TestUtils.requireTask('lang');
+  });
+
+  beforeEach(function(done) {
+    TestUtils.beforeEach(done);
+  });
+
+  after(function(done) {
+    TestUtils.after(done);
+  });
+
+  it('should extract messages for translations', function(done) {
+    TestUtils.runTask('lang', function() {
+      var file = 'src/translations/default.xlf';
+      assert.ok(fs.existsSync(file));
+
+      var contents = fs.readFileSync(file, 'utf8');
+      assert.notStrictEqual(-1, contents.indexOf('Hello'));
+
+      fs.unlinkSync('src/translations/default.xlf');
+      done();
+    });
+  });
+});
