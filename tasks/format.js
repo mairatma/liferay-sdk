ebba20d20833941af51544ee330e3ba5667454fa
Maira Bello <maira.araujo@liferay.com>
2014-10-30 10:11:07 -0300

    Changes `format` task to only work for files inside public folder

--- tasks/format.js
+++ tasks/format.js
@@ -2,13 +2,14 @@
 
 var gulp = require('gulp');
 var es = require('event-stream');
+var path = require('path');
 var plugins = require('gulp-load-plugins')();
 var config = require('../src/flavor/ProductFlavors').generateFlavoredConfig();
 
 gulp.task('format:scripts', function() {
-  var scripts = gulp.src('src/' + config.globScript)
+  var scripts = gulp.src(path.join('src/public/scripts', config.globScript))
     .pipe(plugins.esformatter())
-    .pipe(gulp.dest('src'));
+    .pipe(gulp.dest('src/public/scripts'));
 
   var tasks = gulp.src(['tasks/**/*.js'])
     .pipe(plugins.esformatter())
@@ -22,19 +23,19 @@ gulp.task('format:scripts', function() {
 });
 
 gulp.task('format:styles', function() {
-  var styles = gulp.src('src/' + config.globStyle)
+  var styles = gulp.src(path.join('src/public/styles', config.globStyle))
     .pipe(plugins.csscomb())
     .pipe(plugins.cssbeautify({
       indent: '  '
     }))
-    .pipe(gulp.dest('src'));
+    .pipe(gulp.dest('src/public/styles'));
 
-  var scss = gulp.src('src/' + config.globScss)
+  var scss = gulp.src(path.join('src/public/styles', config.globScss))
     .pipe(plugins.csscomb())
     .pipe(plugins.cssbeautify({
       indent: '  '
     }))
-    .pipe(gulp.dest('src'));
+    .pipe(gulp.dest('src/public/styles'));
 
   return es.merge(styles, scss);
 });
