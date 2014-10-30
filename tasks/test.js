46e5e72b6dd6078fa7cb377eee505af6ef3af6a6
Maira Bello <maira.araujo@liferay.com>
2014-10-30 10:28:37 -0300

    Moves `test:integration` to gulpfile

--- tasks/test.js
+++ tasks/test.js
@@ -1,27 +1,11 @@
 'use strict';
 
 var gulp = require('gulp');
-var path = require('path');
 var plugins = require('gulp-load-plugins')();
-var execFile = require('child_process').execFile;
-
-gulp.task('test:integration', function(done) {
-  var args = ['test/integration/**/*.js', '--slow', '1000', '--timeout', '3000'];
-  var config = {
-    stdio: 'inherit'
-  };
-
-  var localMocha = path.join(process.cwd(), 'node_modules', '.bin', 'mocha');
-
-  var child = execFile(localMocha, args, config, function() {
-    done();
-  });
-  child.stdout.pipe(process.stdout);
-});
 
 gulp.task('test:unit', function() {
   return gulp.src(['test/unit/**/*.js', '!test/unit/**/fixture/**/*.js'])
     .pipe(plugins.mocha());
 });
 
-gulp.task('test', ['test:unit', 'test:integration']);
+gulp.task('test', ['test:unit']);
