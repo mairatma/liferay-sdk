c2d9aea78a4feaa32390694b7e5b642d60d5ec42
Maira Bello <maira.araujo@liferay.com>
2014-10-30 10:11:07 -0300

    Changes `format` task to only work for files inside public folder

--- test/integration/tasks/format.js
+++ test/integration/tasks/format.js
@@ -46,6 +46,19 @@ describe('format', function() {
         done();
       });
     });
+
+    it('should not format src js scripts outside public folder', function(done) {
+      fs.writeFileSync('src/test.js', 'var a = 42;   ');
+
+      TestUtils.runTask('format:scripts', function() {
+        var contents = fs.readFileSync('src/test.js', 'utf8');
+        assert.notStrictEqual('var a = 42;\n', contents);
+        assert.strictEqual('var a = 42;   ', contents);
+
+        fs.unlinkSync('src/test.js');
+        done();
+      });
+    });
   });
 
   describe('format:styles', function() {
@@ -57,8 +70,31 @@ describe('format', function() {
         var contents = fs.readFileSync('src/public/styles/test.css', 'utf8');
         assert.strictEqual('.cssClass {\n  color: black;\n}\n', contents);
 
+        contents = fs.readFileSync('src/public/styles/test.scss', 'utf8');
+        assert.strictEqual('.cssClass {\n  color: black;\n}\n', contents);
+
         fs.unlinkSync('src/public/styles/test.css');
         fs.unlinkSync('src/public/styles/test.scss');
+
+        done();
+      });
+    });
+
+    it('should not format css files outside public folder', function(done) {
+      fs.writeFileSync('src/test.css', '.cssClass {\ncolor: black\n}');
+      fs.writeFileSync('src/test.scss', '.cssClass {\ncolor: black\n}');
+
+      TestUtils.runTask('format:styles', function() {
+        var contents = fs.readFileSync('src/test.css', 'utf8');
+        assert.notStrictEqual('.cssClass {\n  color: black;\n}\n', contents);
+        assert.strictEqual('.cssClass {\ncolor: black\n}', contents);
+
+        contents = fs.readFileSync('src/test.scss', 'utf8');
+        assert.notStrictEqual('.cssClass {\n  color: black;\n}\n', contents);
+        assert.strictEqual('.cssClass {\ncolor: black\n}', contents);
+
+        fs.unlinkSync('src/test.css');
+        fs.unlinkSync('src/test.scss');
 
         done();
       });
